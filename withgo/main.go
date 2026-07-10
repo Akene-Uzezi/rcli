cbb6e1951112d3e081a258606bb466bce073d0f7
Akene-Uzezi <xtremejoexj@gmail.com>
2026-07-10 16:36:52 +0100

    add post command

--- withgo/main.go
+++ withgo/main.go
@@ -5,9 +5,12 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
+
+var headers map[string]any
 
 func main() {
 	// Define the root command
@@ -43,8 +46,59 @@ func main() {
 		},
 	}
 
+	// define variable to capture the flag values
+	var headers []string
+	var data string
+
+	postCmd := &cobra.Command{
+		Use:   "post [url]",
+		Short: "make a post command",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			url := args[0]
+
+			reqBody := strings.NewReader(data)
+
+			req, err := http.NewRequest("POST", url, reqBody)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "failed to initialize request: %v\n", err)
+				os.Exit(1)
+			}
+
+			for _, h := range headers {
+				parts := strings.SplitN(h, ":", 2)
+				if len(parts) == 2 {
+					key := strings.TrimSpace(parts[0])
+					value := strings.TrimSpace(parts[1])
+					req.Header.Add(key, value)
+				}
+			}
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error executing post request: %v\n", err)
+				os.Exit(1)
+			}
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "failed to read post response: %v\n", err)
+				os.Exit(1)
+			}
+
+			fmt.Printf("status: %s\n", resp.Status)
+			fmt.Println(string(body))
+		},
+	}
+
+	postCmd.Flags().StringSliceVarP(&headers, "headers", "H", []string{}, "HTTP headers to pass")
+	postCmd.Flags().StringVarP(&data, "data", "d", "", "the raw string data payload for the POST body")
+
 	// add get command to root command and execute
 	rootCmd.AddCommand(getCmd)
+	rootCmd.AddCommand(postCmd)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
