e1f301aaf283e98a2f687061547d9bc2fc22b565
Akene-Uzezi <xtremejoexj@gmail.com>
2026-07-14 15:07:03 +0100

    removed unused get command

--- withgo/main.go
+++ withgo/main.go
@@ -37,33 +37,6 @@ func main() {
 		},
 	}
 
-	// Define the get subcommand
-	getCmd := &cobra.Command{
-		Use:   "get [url]",
-		Short: "fetch content from a url",
-		Args:  cobra.ExactArgs(1), // requires exactly one argument
-		Run: func(cmd *cobra.Command, args []string) {
-			url := args[0]
-			// perform the http get requires
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "error making request %v\n", err)
-				os.Exit(1)
-			}
-			defer resp.Body.Close()
-
-			// read the response body
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "error reading response %v\n", err)
-				os.Exit(1)
-			}
-
-			fmt.Printf("status: %s\n", resp.Status)
-			fmt.Println(string(body))
-		},
-	}
-
 	// define variable to capture the flag values
 	var headers []string
 	var data string
@@ -115,7 +88,6 @@ func main() {
 	postCmd.Flags().StringVarP(&data, "data", "d", "", "the raw string data payload for the POST body")
 
 	// add get command to root command and execute
-	rootCmd.AddCommand(getCmd)
 	rootCmd.AddCommand(postCmd)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
